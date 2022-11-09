0adaddf96e0ce3c684e8008b19e77d61311eeb7d
Lucas Chagas <107507922+llucaschagass@users.noreply.github.com>
2022-11-08 23:01:56 -0300

    first commit

---
+++ Models/Nokia.cs
@@ -0,0 +1,14 @@
+namespace DesafioPOO.Models
+{
+    public class Nokia : Smartphone
+    {
+        public Nokia(string numero, string modelo, string iMEI, int memoria) : base(numero, modelo, iMEI, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando aplicativo {nomeApp} no Nokia");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "11996542312", modelo: "N95", iMEI: "9012345", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "21993827282", modelo: "Iphone 13", iMEI: "32123455", memoria: 4);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
